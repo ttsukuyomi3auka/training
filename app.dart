6217dc5c899517781c244008bb4a356b01ec246e
tsukuyomi <tsumi@mail>
2023-09-23 09:04:31 +0500

    training dark

--- app.dart
+++ app.dart
@@ -1,1 +1,17 @@
 //Здесь какой-то код!!
+import 'dart:io';
+void main()
+{
+    print ("hello world");
+    var name ="Oleg"; //var сам определяет тип переменной по присвоеному значению
+    print(name);
+    dynamic temp ="Danil";
+    print(temp);
+    temp=45;
+    print(temp); // dynamic сам определяет тип переменной по присвоеному значению, а также способен изменять тип
+    const data="22,09";
+    final current = DateTime.now();
+    print (" $data" " $current" );
+    
+
+}
